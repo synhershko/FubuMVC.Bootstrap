7a576d3f3cad20d9d66df09838cc7612b2e20d1a
synhershko <itamar@code972.com>
2014-03-13 21:30:37 +0200

    Getting bottle ready to be used

--- src/BootstrapDemonstrator/Global.asax
+++ src/BootstrapDemonstrator/Global.asax
@@ -1,1 +1,1 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TwitterBootstrapDemonstrator.Global" Language="C#" %>
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BootstrapDemonstrator.Global" Language="C#" %>
--- src/BootstrapDemonstrator/Properties/AssemblyInfo.cs
+++ src/BootstrapDemonstrator/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TwitterBootstrapDemonstrator")]
+[assembly: AssemblyTitle("BootstrapDemonstrator")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TwitterBootstrapDemonstrator")]
+[assembly: AssemblyProduct("BootstrapDemonstrator")]
 [assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
